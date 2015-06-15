8ec58bd9b04adf0f3eea55c7e4e3fe9a8c8e43f2
amverhagen <amverhagen93@gmail.com>
2015-06-15 12:25:07 -0400

    Remove badlogic.png, Set desktop resolution

--- desktop/src/com/amverhagen/pulsedodge/desktop/DesktopLauncher.java
+++ desktop/src/com/amverhagen/pulsedodge/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.amverhagen.pulsedodge.PulseDodge;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 450;
+		config.height = 800;
 		new LwjglApplication(new PulseDodge(), config);
 	}
 }
