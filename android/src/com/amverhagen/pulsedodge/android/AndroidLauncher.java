4c1c5b9f2ae7df2dd7a6b9b71ed38e4591edcb52
amverhagen <amverhagen93@gmail.com>
2015-06-15 11:14:51 -0400

    Create initial Libgdx project

---
+++ android/src/com/amverhagen/pulsedodge/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.amverhagen.pulsedodge.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.amverhagen.pulsedodge.PulseDodge;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PulseDodge(), config);
+	}
+}
---
+++ desktop/src/com/amverhagen/pulsedodge/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.amverhagen.pulsedodge.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.amverhagen.pulsedodge.PulseDodge;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PulseDodge(), config);
+	}
+}
