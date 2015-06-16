2430fcc231a5d916cc8495011fa5660c7909432b
amverhagen <amverhagen93@gmail.com>
2015-06-16 12:53:51 -0400

    -Modify CircleLine to only show one circle -Implement movement feature for desktop

--- core/src/com/amverhagen/pulsedodge/PulseDodge.java
+++ core/src/com/amverhagen/pulsedodge/PulseDodge.java
@@ -3,27 +3,82 @@ package com.amverhagen.pulsedodge;
 import com.amverhagen.pulsedodge.playcomponents.CircleLine;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class PulseDodge extends ApplicationAdapter {
+public class PulseDodge extends ApplicationAdapter implements InputProcessor {
 	private SpriteBatch batch;
 	private CircleLine circleLine;
-	
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
 		circleLine = new CircleLine(5, 0, Gdx.graphics.getHeight() / 3,
 				Gdx.graphics.getWidth(), Gdx.graphics.getHeight() / 12);
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(0, .5f, .5f, 1);
+		Gdx.gl.glClearColor(0, 0, 0f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		circleLine.draw(batch);
+		circleLine.getCircle().draw(batch);
 		batch.end();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		if (keycode == Keys.D) {
+			circleLine.moveRight();
+		}
+		if (keycode == Keys.A) {
+			circleLine.moveLeft();
+		}
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
--- core/src/com/amverhagen/pulsedodge/playcomponents/CircleLine.java
+++ core/src/com/amverhagen/pulsedodge/playcomponents/CircleLine.java
@@ -3,57 +3,43 @@ package com.amverhagen.pulsedodge.playcomponents;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class CircleLine {
-	private Sprite[] circles;
-	private int circleAmount;
+	private int[] indexPositions;
 	private int activeIndex;
-	private Texture activeCircle;
-	private Texture emptyCircle;
+	private Sprite circle;
 
-	public CircleLine(int circleAmount, int x_pos, int y_pos, int width,
-			int height) {
-		System.out.println("xpos: " + x_pos + ", y_pos: " + y_pos + ", width: "
-				+ width + ", height: " + height);
-		circles = new Sprite[circleAmount];
-		this.circleAmount = circleAmount;
-		int sectionWidth = width / circleAmount;
-		System.out.println("Section width: " + sectionWidth);
-		int circleWidth = sectionWidth / 3;
-		System.out.println("Circle width: " + circleWidth);
-		activeIndex = circleAmount / 2;
-		System.out.println("Active index: " + activeIndex);
-		activeCircle = new Texture(Gdx.files.internal("white_sphere.png"));
-		emptyCircle = new Texture(Gdx.files.internal("empty_black.png"));
-		int circleX = sectionWidth / 2 - circleWidth / 2;
-		for (int i = 0; i < circleAmount; i++) {
-			System.out.println("Index: " + i + ", circleX " + circleX);
-			circles[i] = new Sprite(emptyCircle);
-			circles[i].setBounds(circleX, y_pos, circleWidth, circleWidth);
-			circleX += sectionWidth;
-			System.out.println("Index: " + i + ", xpos: " + circles[i].getX());
+	public CircleLine(int indices, int x_pos, int y_pos, int width, int height) {
+		int circleWidth = width / 12;
+		activeIndex = indices / 2;
+		indexPositions = new int[indices];
+		int sectionWidth = width / indices;
+		for (int i = 0; i < indices; i++) {
+			indexPositions[i] = (sectionWidth / 2) - circleWidth / 2
+					+ (sectionWidth * i);
 		}
-		circles[activeIndex].setTexture(activeCircle);
+
+		circle = new Sprite(new Texture(Gdx.files.internal("green_circle.png")));
+		circle.setBounds(indexPositions[activeIndex], y_pos, circleWidth,
+				circleWidth);
 	}
 
 	public void moveRight() {
-
+		if (activeIndex < 4) {
+			activeIndex++;
+		}
+		circle.setPosition(indexPositions[activeIndex], circle.getY());
 	}
 
 	public void moveLeft() {
-
-	}
-
-	public Sprite getActiveCircle() {
-		return null;
-	}
-
-	public void draw(SpriteBatch batch) {
-		for (int i = 0; i < circleAmount; i++) {
-			batch.draw(circles[i].getTexture(), circles[i].getX(),
-					circles[i].getY(), circles[i].getWidth(),
-					circles[i].getHeight());
+		if (activeIndex > 0) {
+			activeIndex--;
 		}
+		circle.setPosition(indexPositions[activeIndex], circle.getY());
+
+	}
+
+	public Sprite getCircle() {
+		return circle;
 	}
 }
