8cdfdfd9867e0bc993a17e3a86900b84ab38b1ef
amverhagen <amverhagen93@gmail.com>
2015-06-16 15:15:23 -0400

    -Add gesture detection and multiplexor

--- core/src/com/amverhagen/pulsedodge/PulseDodge.java
+++ core/src/com/amverhagen/pulsedodge/PulseDodge.java
@@ -4,6 +4,7 @@ import com.amverhagen.pulsedodge.playcomponents.CircleLine;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
@@ -11,12 +12,18 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.input.GestureDetector.GestureListener;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class PulseDodge extends ApplicationAdapter implements InputProcessor {
+public class PulseDodge extends ApplicationAdapter implements InputProcessor,
+		GestureListener {
+	private InputMultiplexer inputMultiplexer;
 	private final int GAME_WORLD_WIDTH = 900;
 	private final int GAME_WORLD_HEIGHT = 1600;
+	private GestureDetector gestureDetector;
 	private SpriteBatch batch;
 	private CircleLine circleLine;
 	private Viewport viewport;
@@ -25,24 +32,30 @@ public class PulseDodge extends ApplicationAdapter implements InputProcessor {
 
 	@Override
 	public void create() {
+		inputMultiplexer = new InputMultiplexer();
+		gestureDetector = new GestureDetector(this);
+		InputProcessor ip = this;
+		inputMultiplexer.addProcessor(gestureDetector);
+		inputMultiplexer.addProcessor(ip);
 		camera = new OrthographicCamera();
 		camera.position.set(GAME_WORLD_WIDTH / 2, GAME_WORLD_HEIGHT / 2, 0);
 		viewport = new FitViewport(GAME_WORLD_WIDTH, GAME_WORLD_HEIGHT, camera);
 		viewport.apply();
 		batch = new SpriteBatch();
-		Gdx.input.setInputProcessor(this);
-		background = new Sprite(new Texture(Gdx.files.internal("background.png")));
+		background = new Sprite(new Texture(
+				Gdx.files.internal("background.png")));
 		background.setPosition(0, 0);
 		background.setSize(GAME_WORLD_WIDTH, GAME_WORLD_HEIGHT);
 		circleLine = new CircleLine(5, 0f, GAME_WORLD_HEIGHT / 3f,
 				GAME_WORLD_WIDTH, GAME_WORLD_HEIGHT);
 
+		Gdx.input.setInputProcessor(inputMultiplexer);
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		viewport.update(width, height);
-		camera.position.set(GAME_WORLD_WIDTH/2, GAME_WORLD_HEIGHT/2,0);
+		camera.position.set(GAME_WORLD_WIDTH / 2, GAME_WORLD_HEIGHT / 2, 0);
 	}
 
 	@Override
@@ -107,6 +120,63 @@ public class PulseDodge extends ApplicationAdapter implements InputProcessor {
 
 	@Override
 	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(float x, float y, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean tap(float x, float y, int count, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean longPress(float x, float y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean fling(float velocityX, float velocityY, int button) {
+		if (Math.abs(velocityX) > Math.abs(velocityY)) {
+			if (velocityX > 0) {
+				circleLine.moveRight();
+				;
+			} else {
+				circleLine.moveLeft();
+			}
+		}
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean pan(float x, float y, float deltaX, float deltaY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean panStop(float x, float y, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean zoom(float initialDistance, float distance) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2,
+			Vector2 pointer1, Vector2 pointer2) {
 		// TODO Auto-generated method stub
 		return false;
 	}
